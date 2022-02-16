867940db16236b95c9ac27a910a5b695fd9ebf69
lucasquevedo1d <lucasquevedo1d@gmail.com>
2022-02-16 13:31:03 -0300

    corrigindo pr

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,4 @@
+const raçaDecachorros = ['vira-lata','pitbull','buldogue','golden','pug'];
+//                            0           1            2         3     4    
+const numero = prompt('escolha um numero de 1 a 5escolher o cachorro');
+console.log('eu amo'${racaDeCachorro}[numeroDoCachorro-1])
