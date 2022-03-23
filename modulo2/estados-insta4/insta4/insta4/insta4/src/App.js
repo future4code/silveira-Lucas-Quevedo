f0ea504e6d80b910e1cc98dca173c8a912bc502f
lucasquevedo1d <lucasquevedo1d@gmail.com>
2022-03-23 13:36:24 -0300

    nova

--- modulo2/estados-insta4/insta4/insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/insta4/insta4/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import styled from 'styled-components'
-import Post from './components/Post/Post';
+import logo from './logo.svg';
+import './App.css';
 
-const MainContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-`
-
-class App extends React.Component {
-  render() {
-    return (
-      <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-      </MainContainer>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
