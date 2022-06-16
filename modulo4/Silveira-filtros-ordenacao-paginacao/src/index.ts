b11de9e5b255bf6037e4a6042dd4b952f9e92022
lucasquevedo1d <lucasquevedo1d@gmail.com>
2022-06-16 13:34:17 -0300

    ultimo

--- modulo4/Silveira-filtros-ordenacao-paginacao/src/index.ts
+++ modulo4/Silveira-filtros-ordenacao-paginacao/src/index.ts
@@ -2,8 +2,9 @@ import { app } from "./app";
 import { getAllUsers } from "./endpoints/Exercicio1";
 import { getTypeUsers } from "./endpoints/Exercicio2";
 import { getLimitUsers } from "./endpoints/Exercicio3";
+import { getUsers } from "./endpoints/Exercicio4";
 
 app.get("/recipes", getAllUsers)
 app.get("/type",getTypeUsers)
 app.get("/limit", getLimitUsers)
-app.get("/user")
+app.get("/endpoint/user", getUsers)
